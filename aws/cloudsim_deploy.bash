66f2f78216c61c724242aef17fda3117e56454a2
Shane Loretz <sloretz@osrfoundation.org>
2017-01-13 13:27:15 -0800

    Fixed /tmp/.docker.xauth always being blank
    This fix applys when running on a desktop, aws should be unaffected
    Also fixed warning being printed when no arguments are supplied

--- aws/cloudsim_deploy.bash
+++ aws/cloudsim_deploy.bash
@@ -4,11 +4,14 @@
 
 extra_opts=""
 cmd=""
-if [ "--debug" = $1 ]
+if [ $# -eq 1 ]
 then
-    # make it easy to run gdb inside the container
-    extra_opts="-it --security-opt seccomp=unconfined"
-    cmd=bash
+    if [ "--debug" = $1 ]
+    then
+        # make it easy to run gdb inside the container
+        extra_opts="-it --security-opt seccomp=unconfined"
+        cmd=bash
+    fi
 fi
 
 until sudo nvidia-docker ps
@@ -22,23 +25,24 @@ code_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Make sure processes in the container can connect to the x server
 # Necessary so gazebo can create a context for OpenGL rendering (even headless)
-if [ ! -f /tmp/.docker.xauth ]
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
 then
-    export XAUTH=/tmp/.docker.xauth
     xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
-    if [ ! -z $xauth_list ]
+    if [ ! -z "$xauth_list" ]
     then
         echo $xauth_list | xauth -f $XAUTH nmerge -
     else
-        touch /tmp/.docker.xauth
+        touch $XAUTH
     fi
+    chmod a+r $XAUTH
 fi
 
 # Display is hard-coded to :0 because that's what the startup scripts on AWS will generate
 eval sudo nvidia-docker run \
   -e DISPLAY=unix:0 \
-  -e XAUTHORITY=/tmp/.docker.xauth \
-  -v "/tmp/.docker.xauth:/tmp/.docker.xauth" \
+  -e XAUTHORITY=$XAUTH \
+  -v "$XAUTH:$XAUTH" \
   -v "/tmp/.X11-unix:/tmp/.X11-unix" \
   -v "/etc/localtime:/etc/localtime:ro" \
   -v "$code_dir:/code:ro" \
