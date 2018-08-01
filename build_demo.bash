634da65d96a71927bee644fbb0786327fff3153a
RyodoTanaka <groadpg@gmail.com>
2018-08-01 14:52:08 +0900

    [update] delete arguments

--- build_demo.bash
+++ build_demo.bash
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo docker build -t osrf/car_demo $DIR --build-arg DOCKER_CMD='client 172.17.0.1'
+sudo docker build -t osrf/car_demo $DIR
