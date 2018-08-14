ac623bb4b52f99b29775831638b3690a01bfb9d9
RyodoTanaka <groadpg@gmail.com>
2018-08-14 12:18:24 +0900

    [add] converter

---
+++ prius_msg_converter/include/to_cmd_vel.hpp
@@ -0,0 +1,56 @@
+#ifndef TO_CMD_VEL_HPP_INCLUDED
+#define TO_CMD_VEL_HPP_INCLUDED
+
+// usual header for ROS
+#include <ros/ros.h>
+
+// messages to use
+#include <prius_msgs/Control.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+
+// for std lib
+#include <iostream>
+#include <mutex>
+
+
+namespace prius {
+  namespace msg_converter {
+    class ToCmdVel {
+    public:
+      ToCmdVel();
+      ~ToCmdVel();
+      void CtrlUpdate();
+    private:
+      void CmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg);
+      void CurStateCallback(const nav_msgs::Odometry::ConstPtr &msg);
+
+      // node handler
+      ros::NodeHandle nh_;
+      
+      // subscriber
+      ros::Subscriber cmd_vel_sub_;
+      ros::Subscriber current_state_sub_;
+      // publisher
+      ros::Publisher control_msg_pub_;
+
+      // tmp_messages
+      geometry_msgs::Twist cmd_vel_;
+      nav_msgs::Odometry current_state_;
+
+      // subscribe message mutex
+      std::mutex cmd_vel_mtx_;
+      std::mutex current_state_mtx_;
+      
+      // parameters
+      double wheel_base_;
+      double max_steer_angle_;
+      double max_vel_;
+      double k_throttle_;
+      double k_brake_;
+      double pub_rate_;
+    };
+  }
+}
+
+#endif
---
+++ prius_msg_converter/src/to_cmd_vel.cpp
@@ -0,0 +1,153 @@
+#include <to_cmd_vel.hpp>
+
+#include <cmath>
+
+namespace pmc = prius::msg_converter;
+
+pmc::ToCmdVel::ToCmdVel():
+  wheel_base_(3.0),
+  max_steer_angle_(0.6458),
+  max_vel_(38.0),
+  k_throttle_(0.1),
+  k_brake_(0.1),
+  pub_rate_(25.0)
+{
+  nh_.param<double>(ros::this_node::getName() + "/wheel_base", wheel_base_, wheel_base_);
+  nh_.param<double>(ros::this_node::getName() + "/max_steer_angle", max_steer_angle_, max_steer_angle_);
+  nh_.param<double>(ros::this_node::getName() + "/max_velocity", max_vel_, max_vel_);
+  nh_.param<double>(ros::this_node::getName() + "/k_throttle", k_throttle_, k_throttle_);
+  nh_.param<double>(ros::this_node::getName() + "/k_brake", k_brake_, k_brake_);
+  nh_.param<double>(ros::this_node::getName() + "/publish_rate", pub_rate_);
+
+  control_msg_pub_ = nh_.advertise<prius_msgs::Control>("/prius", 1);
+
+  cmd_vel_sub_ = nh_.subscribe("/cmd_vel", 1, &pmc::ToCmdVel::CmdVelCallback, this);
+  current_state_sub_ = nh_.subscribe("/base_pose_ground_truth", 1, &pmc::ToCmdVel::CurStateCallback, this);
+}
+
+pmc::ToCmdVel::~ToCmdVel(){ }
+
+void pmc::ToCmdVel::CmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg)
+{
+  cmd_vel_mtx_.lock();
+  cmd_vel_ = *msg;
+  cmd_vel_mtx_.unlock();
+}
+
+void pmc::ToCmdVel::CurStateCallback(const nav_msgs::Odometry::ConstPtr &msg)
+{
+  current_state_mtx_.lock();
+  current_state_ = *msg;
+  current_state_mtx_.unlock();
+}
+
+void pmc::ToCmdVel::CtrlUpdate()
+{  
+  static ros::Rate r(pub_rate_);
+  static prius_msgs::Control ctrl_msg;
+  
+  while(ros::ok()){
+    ros::spinOnce();
+    
+    // get current state
+    static double current_vel = 0;
+    current_state_mtx_.lock();
+    current_vel = sqrt(pow(current_state_.twist.twist.linear.x,2)+pow(current_state_.twist.twist.linear.y,2));
+    current_state_mtx_.unlock();
+
+    // get objective velocity
+    static double obj_vel = 0;
+    static double obj_ang_vel = 0;
+    cmd_vel_mtx_.lock();
+    obj_vel = cmd_vel_.linear.x;
+    obj_ang_vel = cmd_vel_.angular.z;
+    cmd_vel_mtx_.unlock();
+
+    std::cout << "obj_vel : ";
+    std::cout << obj_vel << std::endl;
+    std::cout << "current_vel :";
+    std::cout << current_vel << std::endl;
+    
+    // set time stamp for control input
+    ctrl_msg.header.stamp = ros::Time::now();
+    
+    // calculate control steer angle
+    static double ctrl_angle = 0;
+    ctrl_angle = atan((wheel_base_*obj_ang_vel)/current_vel)/(2.0*max_steer_angle_);
+    ctrl_angle = std::fabs(ctrl_angle) > 1.0 ? ctrl_angle/std::fabs(ctrl_angle) : ctrl_angle;
+
+    ctrl_msg.steer = ctrl_angle;
+
+    // calculate throttle & shift gear input
+    if(obj_vel==0){
+      ctrl_msg.throttle = 0.0;
+      ctrl_msg.brake = 1.0;
+      ctrl_msg.steer = 0.0;
+      std::cout << "brake :";
+      std::cout << ctrl_msg.brake << std::endl;
+      std::cout << "throttle : ";
+      std::cout << ctrl_msg.throttle << std::endl;
+      control_msg_pub_.publish(ctrl_msg);
+      r.sleep();
+    }
+    else if(obj_vel>0){
+      ctrl_msg.throttle = 0.0;
+      ctrl_msg.brake = 0.0;
+      ctrl_msg.shift_gears = prius_msgs::Control::FORWARD;
+    }
+    else{
+      ctrl_msg.throttle = 0.0;
+      ctrl_msg.brake = 0.0;
+      ctrl_msg.shift_gears = prius_msgs::Control::REVERSE;
+    }
+      
+    static double err_vel = 0;
+    static double ctrl_vel = 0;
+    err_vel = std::fabs(obj_vel) - current_vel;
+    std::cout << "err_vel : ";
+    std::cout << err_vel << std::endl;
+    
+    switch(ctrl_msg.shift_gears){
+    case prius_msgs::Control::FORWARD:
+      if(0<err_vel){
+        ctrl_vel = k_throttle_ * std::fabs(err_vel);
+        ctrl_vel = std::fabs(ctrl_vel) >= 1.0 ? 1.0 : ctrl_vel;
+        ctrl_msg.throttle = ctrl_vel;
+      }
+      else{
+        ctrl_vel = k_brake_ * std::fabs(err_vel);
+        ctrl_vel = std::fabs(ctrl_vel) >= 1.0 ? 1.0 : ctrl_vel;
+        ctrl_msg.brake = ctrl_vel;   
+      }
+      break;
+    case prius_msgs::Control::REVERSE:
+      if(0<err_vel){
+        ctrl_vel = k_throttle_ * std::fabs(err_vel);
+        ctrl_vel = std::fabs(ctrl_vel) >= 1.0 ? 1.0 : ctrl_vel;
+        ctrl_msg.throttle = ctrl_vel;
+      }
+      else{
+        ctrl_vel = k_brake_ * std::fabs(err_vel);
+        ctrl_vel = std::fabs(ctrl_vel) >= 1.0 ? 1.0 : ctrl_vel;
+        ctrl_msg.brake = ctrl_vel;   
+      }
+      break;
+    default:
+      break;
+    }
+ 
+    std::cout << "brake :";
+    std::cout << ctrl_msg.brake << std::endl;
+    std::cout << "throttle : ";
+    std::cout << ctrl_msg.throttle << std::endl;
+  
+    // publish the ctrl msg
+    control_msg_pub_.publish(ctrl_msg);
+    r.sleep();
+
+  }
+}
+
+
+
+
